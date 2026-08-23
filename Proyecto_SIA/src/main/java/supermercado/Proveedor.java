960ae3c0e6663862f491ed8c53e71bb753249d4e
ignac <ignac@Nachoo>
2026-08-23 18:17:23 -0400

    Avance proovedor

--- Proyecto_SIA/src/main/java/supermercado/Proveedor.java
+++ Proyecto_SIA/src/main/java/supermercado/Proveedor.java
@@ -10,5 +10,27 @@ package supermercado;
  * @author ignac
  */
 public class Proveedor {
+    private  String rut;
+    private  String razonSocial;
+    private  String telefono;
+    
+    public Proveedor(String rut, String razonSocial, String telefono){
+        this.rut = rut;
+        this.razonSocial = razonSocial;
+        this.telefono = telefono;
+    }
+    
+    public String getRut(){ return rut;}
+    public void setRut(String rut){
+        this.rut = rut;
+    }
+    public String getRazonSocial(){return razonSocial;}
+    public void setRazonSocial(String razonSocial){
+        this.razonSocial = razonSocial;
+    }
+    public String getTelefono(){return telefono;}
+    public void setTelefono(String telefono){
+        this.telefono = telefono;
+    }
     
 }
