16b66478788d35b86c52e583c771b1277d1d3a81
ignac <ignac@Nachoo>
2026-08-23 18:40:48 -0400

    Avance DetalleOrdenCompra

--- Proyecto_SIA/src/main/java/supermercado/DetalleOrdenCompra.java
+++ Proyecto_SIA/src/main/java/supermercado/DetalleOrdenCompra.java
@@ -10,5 +10,24 @@ package supermercado;
  * @author ignac
  */
 public class DetalleOrdenCompra {
+    private Producto producto;
+    private int cantidad;
     
+    public DetalleOrdenCompra(Producto producto, int cantidad){
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+    
+    //falta detalleOrdenCompra//
+    
+    
+    public Producto getProducto(){return producto;}
+    public void setProducto(Producto producto){
+        this.producto = producto;
+    }
+    public int getCantidad(){return cantidad;}
+    public void setCantidad(int cantidad){
+        this.cantidad = cantidad;
+    }
+   
 }
