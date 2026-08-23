a44bb4be2f0389fe25fdf02f7eb0250c8f8a3802
ignac <ignac@Nachoo>
2026-08-23 18:48:31 -0400

    Avance Venta

--- Proyecto_SIA/src/main/java/supermercado/Venta.java
+++ Proyecto_SIA/src/main/java/supermercado/Venta.java
@@ -4,11 +4,27 @@
  * and open the template in the editor.
  */
 package supermercado;
-
+import java.util.HashMap;
 /**
  *
  * @author ignac
  */
 public class Venta {
+    
+    private String idVenta;
+    private String fecha;
+    private HashMap<String, Integer> productosVendidos;
+    
+    public Venta(String idVenta, String fecha){
+        this.idVenta = idVenta;
+        this.fecha = fecha;
+        this.productosVendidos = new HashMap<>();
+    }
+    //falta AgregarProductos
+    //falta calcularTotal
+    //falta procesarVenta
+    public String getIdVenta(){return idVenta;}
+    public String getFecha(){return fecha;}
+    public HashMap<String,Integer> getProductos(){return productosVendidos;}
     
 }
