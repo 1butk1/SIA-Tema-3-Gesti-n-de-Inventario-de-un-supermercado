ffdcba61e2d7bf0aca486dab9f710b3fb0046b2c
ignac <ignac@Nachoo>
2026-08-23 18:33:38 -0400

    Avance ordenCompra

--- Proyecto_SIA/src/main/java/supermercado/OrdenCompra.java
+++ Proyecto_SIA/src/main/java/supermercado/OrdenCompra.java
@@ -4,11 +4,31 @@
  * and open the template in the editor.
  */
 package supermercado;
-
+import java.util.ArrayList;
 /**
  *
  * @author ignac
  */
 public class OrdenCompra {
+    private String idOrden;
+    private String fecha;
+    Proveedor proveedor;
+    private ArrayList<DetalleOrdenCompra> detalles;
+   //falta completar ordenCompra//
+    public OrdenCompra(String idOrden, String fecha, Proveedor proveedor){
+        this.idOrden = idOrden;
+        this.fecha = fecha;
+    }
+    // falta agregarDetalle//
+    //falta generarReporte//
+  
+    public String getIdOrden(){ return idOrden;}
+    public String getFecha(){ return fecha;}
+    public Proveedor getProveedor(){return proveedor;}
+    public ArrayList getDetalles(){return detalles;}
+    
+    
+    // falta toString//
+    
     
 }
