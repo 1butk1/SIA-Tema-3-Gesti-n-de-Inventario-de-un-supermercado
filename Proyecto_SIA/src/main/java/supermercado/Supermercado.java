9bda1179634506bd03177c44d95348102433687b
ignac <ignac@Nachoo>
2026-09-08 13:25:20 -0300

    Finalizacion de Clase Supermercado

--- Proyecto_SIA/src/main/java/supermercado/Supermercado.java
+++ Proyecto_SIA/src/main/java/supermercado/Supermercado.java
@@ -29,11 +29,9 @@ public class Supermercado {
     public void agregarSeccion(Seccion s) {
         this.secciones.put(s.getCodigo(), s);
     }
-
     public Seccion buscarSeccion(String codigo) {
         return this.secciones.get(codigo);
     }
-    
     public Producto buscarProducto(String codigoProducto) {
         for (Seccion s : secciones.values()) {
             Producto p = s.buscarProducto(codigoProducto);
@@ -43,7 +41,6 @@ public class Supermercado {
         }
         return null; 
     }
-    
     public Producto buscarProducto(String nombreProducto, String codigoSeccion) {
         Seccion s = this.buscarSeccion(codigoSeccion);
         if (s != null) {
@@ -55,7 +52,46 @@ public class Supermercado {
         }
         return null; 
     }
+    public boolean eliminarSeccion(String codigo){
+        return this.secciones.remove(codigo) != null;
+    } 
     
+    public void verificarStockYGenerarOrdenes(){
+        for(Seccion s : secciones.values()){
+            for(Producto p : s.getProductos().values()){
+                if(p.getStock() <= p.getPuntoReOrden()){
+                    System.out.println("!ALERTA : El producto "+ p.getNombre()+"requiere rebastecimiento. Stock Actual: "+p.getStock()+"\n");
+                }
+            }
+        }
+    }
+    public String generarReportesOrdenesPorSeccion(String codigoSeccion){
+        Seccion s = buscarSeccion(codigoSeccion);
+        if(s == null){return "!ERROR : SECCION NO ENCONTRADA";}
+        String reporte = "**** REPORTE DE ORDENES POR SECCION ****\n";
+        reporte += "SECCION : "+s.getNombre()+" CODIGO: "+ s.getCodigo()+"\n";
+        
+        boolean hayProductos = false;
+        for(Producto p : s.getProductos().values()){
+            if(p.requiereRebastecimiento()){
+                reporte+= "* Produco:"+p.getNombre()+"|Stock actual: "+ p.getStock()+"|Stock Minimo: "+p.getStockMinimo()+ "\n";
+                hayProductos = true;
+            }
+        }
+        if(!hayProductos){
+        reporte += "*** Todos los productos de esta seccion tienen stock ***\n";
+        }
+        return reporte;
+    }
+    public String generarReporteOrdenesPorProveedor(String rutProveedor){
+        String reporte = "**** REPORTE DE ORDENES POR PROVEEDOR ****\n";
+        reporte += "Rut proveedor: "+ rutProveedor+"\n\n";
+        
+        reporte += "Consulta realizada para el proveedor"+ rutProveedor+"\n";
+        return reporte;
+    }
+
+  
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
     public String getRut() { return rut; }
